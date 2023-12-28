a15451a6bb4af29c80927142d7c4141b8f758611
NHANH23 <lamngocanh060203@gmail.com>
2023-12-28 18:27:45 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,201 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+import requests
+from kivymd.color_definitions import colors,palette
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.screen import MDScreen
+from kivy.properties import StringProperty,NumericProperty, ColorProperty,ListProperty
+from kivy.metrics import dp
+import cv2
+# Window.size = (350,900)
+from utils import KivyCamera,SnackbarManager
+from kivy.uix.boxlayout import BoxLayout
+
+
+
+allow_action_list =  {
+    "running":["performance","balanced","mode","stop"],
+    "stop":["init"],
+    "ready":["start"]
+}
+
+database = {
+    "user1" :{
+        "email": "anhnh.t1.1821@gmail.com",
+        "status":"stop",
+        "detail":{
+            "rtsp":f"",
+        }
+    },
+    "user2" :{
+        "email": "lamngocanh060203@gmail.com",
+        "status":"running",
+        "detail":{
+            "rtsp":f"",
+        }
+        
+    }
+}
+
+class HomeScreen(MDScreen):
+    pass
+class SettingsScreen(MDScreen):
+    pass
+
+    
+class MainApp(MDApp):
+    def __init__(self, **kwargs):
+        super(MainApp, self).__init__(**kwargs)
+        Builder.load_file('kv/utils.kv') 
+        Builder.load_file('kv/home.kv') 
+        Builder.load_file('kv/settings.kv') 
+        
+        
+        self.snackbarManager = SnackbarManager()
+        self.user = database["user1"]
+        self.current_page = "home_screen"
+    def build(self):
+        return Builder.load_file('kv/main.kv') 
+    def on_start(self):
+        self._enabledWithStatus(self.user)
+        self.root.ids.home.ids.sideBar.set_state()
+        
+    def _init_camera(self,rtps):
+        try:
+            self.capture = cv2.VideoCapture(rtps)
+            self.my_camera = KivyCamera(capture=self.capture, fps=2)
+        except Exception as e:
+            return False
+        return True
+
+    def on_stop(self):
+        if self.capture:
+            self.capture.release()
+        
+
+    def _auth_session(self, session):
+        # check database status của username
+        return True
+    def show_dropdown(self,instance):
+        
+        menu_items = [
+            {
+                "viewclass": "IconListItem",
+                "icon": "home-variant",
+                "height": dp(56),
+                "text": f"Nhà",
+                "bg_color":(0,1,1,1) if self.current_page=="home_screen" else (0,1,1,0.2),
+                "font_style":"H6",
+                "on_release": lambda :self.redirect_page("home_screen"),
+        
+            } ,
+            {
+                "viewclass": "IconListItem",
+                "icon": "cog",
+                "height": dp(56),
+                "text": f"Cài Đặt",
+                "bg_color":(0,1,1,1) if self.current_page=="settings_screen" else (0,1,1,0.2),
+                "on_release": lambda :self.redirect_page("settings_screen"),
+                
+            }
+            ]
+        self.menu = MDDropdownMenu(
+            caller=instance,
+            items=menu_items,
+            position="bottom",
+            width_mult=3,
+            background_color= "pink",
+            opening_time=0.3,
+        )
+        self.menu.open()
+    def redirect_page(self, place):
+        self.root.ids.screen_manager.current = place
+        self.current_page = place
+        self.menu.dismiss()
+ 
+    def _enabledWithStatus(self,session):
+        if not self._auth_session(session):
+            return False
+        
+        for id_btn,button in self.root.ids.home.ids.items():
+            if id_btn.startswith("abtn"):
+                if id_btn.rsplit("_",1)[-1] in allow_action_list[session["status"]]:
+                    self._enabledButton(button,colors["Orange"]["300"])
+                else:
+                    self._disabledButton(button)
+        return True
+        
+    def on_press(self,button):
+        button.md_bg_color = colors["Orange"]["600"]
+        
+    def _disabledButton(self,button):
+        button.disabled = True
+        button.line_width=1
+    def _enabledButton(self,button,color):
+        button.disabled = False
+        button.md_bg_color = color
+        button.line_width=2
+        
+    def _disabledAllActionButton(self,session):
+        if not self._auth_session(session):
+            return False
+        
+        for id_btn,button in self.root.ids.home.ids.items():
+            if id_btn.startswith("abtn"):
+                self._disabledButton(button)
+        return True
+    
+    def sidebar_abtn_pressed(self,action):
+        self._disabledAllActionButton(session=self.user)
+
+        try:
+            match action:
+                case "init":
+                    response = requests.post("http://127.0.0.1:5005/")
+                case "start":   
+                    response = requests.post("http://127.0.0.1:5005/start")
+                case "stop":   
+                    response = requests.post("http://127.0.0.1:5005/stop")
+      
+                case "performance":   
+                    response = requests.post("http://127.0.0.1:5005/performance")
+         
+            response=response.json()
+                
+        except Exception as e:
+            response = {
+                "status_code":0,
+                "message":"Something went wrong",
+                "detail":{
+                    "message":e
+                }
+            }
+            
+        self.filter_response(response,action)
+        self._enabledWithStatus(session=self.user)
+        return response
+    
+    
+    def filter_response(self, response,action):
+
+        if response["status_code"] == 0:
+            self.snackbarManager.show_snackBar("warning",response,True)
+        elif response["status_code"] == 404:
+            self.snackbarManager.show_snackBar("failed",response,True)
+        elif response["status_code"] ==200:
+            self.snackbarManager.show_snackBar("success",response)
+            self.user["status"] = response["status"]
+            
+            match action:
+                case "init":
+                    rtsp = self.user["detail"]["rtsp"]
+                    is_ok = self._init_camera(rtsp)
+                case "start":
+                    self.root.ids.home.ids.camera.add_widget(self.my_camera)
+                case "stop":
+                    self.root.ids.home.ids.camera.remove_widget(self.my_camera)
+                    self.capture.release()
+                    
+                    
+if __name__ == '__main__':
+    MainApp().run()
---
+++ utils.py
@@ -0,0 +1,138 @@
+from typing import Any
+from kivymd.color_definitions import colors
+from kivymd.uix.button import MDFlatButton
+from kivy.properties import StringProperty,NumericProperty, ColorProperty,ListProperty
+from kivymd.uix.list import OneLineIconListItem
+from kivy.metrics import dp
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.graphics.texture import Texture
+import cv2
+from kivymd.uix.snackbar import BaseSnackbar
+from kivy.animation import Animation
+
+class IconListItem(OneLineIconListItem):
+    icon = StringProperty()
+
+
+
+class CustomSnackbar(BaseSnackbar):
+    text = StringProperty(None)
+    icon = StringProperty(None)
+    font_size = NumericProperty("15sp")
+    text_color = StringProperty("#FFFFFF")
+
+class KivyCamera(Image):
+    def __init__(self, capture, fps, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        self.capture = capture
+        Clock.schedule_interval(self.update, 1.0 / fps)
+
+    def update(self):
+        for _ in range(5):
+            ret, frame = self.capture.read()
+        if ret:
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tostring()
+            image_texture = Texture.create(
+                size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            self.texture = image_texture
+            
+            
+            
+class SnackbarManager:
+    def __init__(self, *args: Any, **kwds: Any) -> Any:
+        self.snackBar = self.snackBar_Success(message="No message!!!")
+    def __init_snackBar(self,type,info,show_detail=False):
+        message = info["message"] 
+        detail = info["detail"] if show_detail and info.get("detail",{}) else {}
+        if type == "success":
+            snackBar = self.snackBar_Success(message,detail)
+            snackBar.md_bg_color = "#66FF33"
+        elif type == "failed":
+            snackBar = self.snackBar_Failed(message,detail)
+            snackBar.md_bg_color = "#FFCC00"
+        elif type == "warning":
+            snackBar = self.snackBar_Warning(message,detail)
+            snackBar.md_bg_color = "#FF3333"
+
+        return snackBar
+  
+    def die(self,instance):
+        anim = Animation(top=0, duration=.3, t='out_quad')
+        anim.bind(on_complete=lambda *args: Window.remove_widget(instance))
+        anim.start(instance)
+        
+    def open(self,instance):
+        Window.add_widget(instance)
+        anim = Animation(y=10, duration=.3, t='out_quad')
+        anim.start(self.snackBar)
+        Clock.schedule_once(lambda dt: self.die(instance), 3)
+    def show_snackBar(self,type,info,show_detail=False):
+        self.die(self.snackBar)
+        self.snackBar = self.__init_snackBar(type,info,show_detail)
+        self.open(self.snackBar)
+        
+
+
+    def snackBar_Success(self,message="Success!",detail={}):
+        snackBar_success =  CustomSnackbar(
+            text=message,
+            icon="check-circle",
+            text_color="white",
+            
+        )
+        snackBar_success.buttons = [
+            MDFlatButton(
+                text="CANCEL",
+                md_bg_color=(1,1,1,0.3),
+                on_release=lambda x:self.die(snackBar_success),
+            ),
+        ]
+        return snackBar_success
+
+
+
+    def snackBar_Failed(self,message = "Failed",detail={}):
+
+        snackBar_failed = CustomSnackbar(
+            text=message,
+            icon="alert-circle",
+            text_color="black",
+        )
+        snackBar_failed.buttons = [
+            MDFlatButton(
+                text="MORE DETAIL",
+                text_color="black",
+                md_bg_color=(1,1,1,0.3),
+                on_release=lambda x:self.show_snackBar("failed",detail)
+            ) if detail else MDFlatButton(),
+            MDFlatButton(
+                text="CANCEL",
+                md_bg_color=(1,1,1,0.3),
+                on_release=lambda x:self.die(snackBar_failed),
+            ),
+        ]
+        return snackBar_failed
+
+    def snackBar_Warning(self,message="Warning",detail={}):
+        snackBar_warning = CustomSnackbar(
+            text=message,
+            icon="cancel",
+            text_color="white",
+        )
+        snackBar_warning.buttons = [
+            MDFlatButton(
+                text="MORE DETAIL",
+                md_bg_color=(1,1,1,0.3),
+                on_release=lambda x:self.show_snackBar("warning",detail) 
+            ) if detail else MDFlatButton(),
+            MDFlatButton(
+                text="CANCEL",
+                md_bg_color=(1,1,1,0.3),
+                on_release=lambda x:self.die(snackBar_warning),
+            ),
+        ]
+        return snackBar_warning
